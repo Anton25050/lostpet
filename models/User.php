0a20672739bfd85cf9b096cfb1462e62010f1154
Student <student@kol.tech.lan>
2024-03-04 12:38:07 +0300

    сделал красивую форму и добавил валидацию пользователя

--- models/User.php
+++ models/User.php
@@ -74,4 +74,75 @@ class User extends \yii\db\ActiveRecord
     {
         return $this->hasOne(Role::class, ['id' => 'role_id']);
     }
+    public static function findIdentity($id)
+    {
+        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        foreach (self::$users as $user) {
+            if ($user['accessToken'] === $token) {
+                return new static($user);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $login
+     * @return static|null
+     */
+    public static function findByUsername($login)
+    {
+        foreach (self::$users as $user) {
+            if (strcasecmp($user['login'], $login) === 0) {
+                return new static($user);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return $this->null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->null;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+
 }
