5bfddd553af5253fb9a5dcbcb6d0dd37832af497
Student <student@kol.tech.lan>
2024-03-06 15:24:47 +0300

    начал делать главную страницу

--- views/site/index.php
+++ views/site/index.php
@@ -2,52 +2,112 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = 'Lostpet';
 ?>
-<div class="site-index">
+<style>
+    .img {
+  display: inline-block;
+  max-width: 18%;
+  height: auto;
+  position: relative;
+  left: 900px;
+  top: -250px;
+}
+.img2 {
+  display: inline-block;
+  max-width: 80%;
+  height: auto;
+  position: relative;
+  left: 200px;   
+  top: 7px;
+}
+.img3 {
+  display: inline-block;
+  max-width: 5%;
+  height: auto;
+  position: relative;
+  left: -320px;
+  top: -5px;
+}
+.img4 {
+display: inline-block;
+  max-width: 6%;
+  height: auto;
+  position: relative;
+  left: -390px;
+  top: 47px; 
+}
+.img5 {
+display: inline-block;
+  max-width: 5%;
+  height: auto;
+  position: relative;
+  left: -462px;
+  top: 87px; 
+}
+    .pos {
+  display: inline-block;
+  position: relative;
+  left: 90px;
+  top: 20px;  
+    }
 
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+    .tech {
+    line-height: 2.8;
+    }
+    .mem {
+    background-size: auto;
+    background: #C0C0C0;
+    border-radius: 5px;
+    }
+</style>
+<div class="container pos">
+  <span class="fs-1 fw-bolder"><span class="text-danger">Lost</span><span class="text-black">pet</span></span>
 </div>
+    <div class='container pos '>
+        <div class='fs-6 text-muted text-wrap' style="width: 25rem;">Найдем и ликвидируем</div>
+    </div>
+        <div class='container pos'>
+        <div class="img2"><img src="web\images\1.jpg" class='img-thumbnail' alt="..."></div>  
+        </div>
+            <div class="container pos">
+            <div class='fs-3 fw-semibold'>О нас</div>
+    <div class="row align-items-start">
+
+        <div class="col">
+        <div class="text-muted text-wrap tech" style="width: 18rem;">
+        У нас работает много волантеров
+        Процент нахождения питомца 86%
+        Активные администраторы сайта    
+        </div>
+        </div>
+        <div class="img3"><img src="web\images\invalid.png" class='img-fluid' alt="...">
+    </div>
+        <div class="img4"><img src="web\images\test.png" class='img-fluid' alt="...">
+    </div>
+        <div class="img5"><img src="web\images\park.png" class='img-fluid' alt="...">
+    </div> 
+        <div class="col-5 tech">
+        <div class="text-muted text-wrap" style="width: 13rem;">
+        Оплата картой
+        Вызов врача на дом
+        Для детей
+        </div>
+        </div>
+        <div class="col tech">
+        <div class="text-muted text-wrap" style="width: 13rem;">
+        Анонимное обследование
+        Массаж
+        </div>
+        </div>
+    </div>
+    </div>
+        <div class='container pos'>
+        <div class='fs-3 fw-semibold'>Наши преимущества</div>
+        <div class="row align-items-center bg-light" style='width:65rem; border-radius: 8px'>
+            <p class= 'fs-6 text-wrap' style='width:60rem;'><span class='fw-bold'>Широкий спектр услуг.</span>Медицинский центр работает в формате универсальной семейной клиники. В сферу нашей деятельности входят все виды медицинской помощи: диагностика, лечение, восстановление и профилактика.</p>
+            <p class= 'fs-6 text-wrap' style='width:60rem;'><span class='fw-bold'>Профессионализм и большой опыт работы.</span>Прием пациентов ведут квалифицированные врачи, за плечами которых длительная практика и широкие знания в своей сфере. В штате медицинских центров работают врачи высшей категории, 40 кандидатов медицинских наук, 4 доктора медицинских наук и 2 профессора.</p>
+            <p class= 'fs-6 text-wrap' style='width:60rem;'><span class='fw-bold'>Современное оборудование.</span>Благодаря хорошему оснащению клиники мы предлагаем пациентам разные виды исследований: КТ, МРТ, рентген, УЗИ, эндоскопия, функциональная диагностика, лабораторные анализы. В кабинетах специалистов есть необходимое оборудование, которое позволяет поставить диагноз уже во время приема.</p>
+            <p class= 'fs-6 text-wrap' style='width:60rem;'><span class='fw-bold'>Высокий уровень обслуживания.</span>Мы стараемся сделать посещение медицинского центра комфортным для каждого пациента. Филиалы работают по удобному графику, у нас нет очередей. Персонал клиники — вежливые и деликатные сотрудники.</p>
+    </div>
+    </div>
